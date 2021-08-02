86cb93a1c4ac7fb16ea804098bba6bed7007ecbc
MohammadJafri <87815696+MohammadJafri@users.noreply.github.com>
2021-08-02 17:31:50 +0530

    Add files via upload

---
+++ Project emotion to emojies home work/main.js
@@ -0,0 +1,74 @@
+prediction_1 =""
+prediction_2=""
+
+Webcam.set({
+    height:300,
+    width:350,
+    image_format:'png',
+    image_quality: 90
+});
+
+camera = document.getElementById("camera");
+Webcam.attach("#camera");
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+
+        
+    });
+}
+console.log('ml5 version:', ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/model.json',modelLoded);
+
+function modelLoaded()
+{
+    console.log("ModelLoaded!");
+}
+
+function speak()
+{
+   var synth = window.speechSynthesis;
+   speech_data_1 = "The first predection is" +prediction_1;
+   speech_data_2 = "And The second predection is" +prediction_2;
+   var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+   synth.speak(utterThis);
+
+}
+
+function check()
+{
+    img=document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results )
+{
+    if (error)
+    {
+        console.error(error);
+    }
+    else
+    {
+        console.log(results);
+        document.getElementById("results_emotion_name").innerHTML = results[0].label;
+        document.getElementById("results_emotion_name2").innerHTML = results[0].label;
+        prediction_1 = results[0].label;
+        prediction_2 = results[0].label;
+        speak();
+        if(results[0].label=="Amazing")
+        {
+          document.getElementById("update_emoji").innerHTML ="&#128076;";
+        }
+        if(results[0].label=="Best")
+        {
+          document.getElementById("update_emoji").innerHTML ="&#128077;";
+        }
+        if(results[0].label=="Victory")
+        {
+          document.getElementById("update_emoji").innerHTML = "&#9996;";
+        }
+    }
+}
